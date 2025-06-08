f09371b4386237ad0f4c0ea8fa9717999ce28c90
HurricaneLily <jostin27022023@gmail.com>
2025-06-08 17:59:34 +0400

    Routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from '../layout/Layout';
+import MainPage from '../pages/main/MainPage';
+import DiscoverPage from '../pages/discover/DiscoverPage';
+import PlaylistPage from '../pages/playlists/PlaylistPage';
+import UserTracksPage from '../pages/user-tracks/UserTracksPage';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <MainPage />,
+        },
+        {
+          path: "/discover",
+          element: <DiscoverPage />,
+        },
+        {
+          path: "/playlists",
+          element: <PlaylistPage />,
+        },
+        {
+          path: "/user-tracks",
+          element: <UserTracksPage />,
+        },
+      ],
+    },
+  ]);
+
 function App() {
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
--- src/layout/components/NavBar.tsx
+++ src/layout/components/NavBar.tsx
@@ -1,6 +1,17 @@
+import { NavLink } from 'react-router-dom';
+
+const activeClass = "text-red-500";
+const notActiveClass = "text-black";
+
 function NavBar () {
     return (
         <nav>
+            <ul>
+                <NavLink to="/" className={({ isActive }) => (isActive ? activeClass : notActiveClass)}>Home</NavLink>
+                <NavLink to="/discover" className={({ isActive }) => (isActive ? activeClass : notActiveClass)}>discover</NavLink>
+                <NavLink to="/playlists" className={({ isActive }) => (isActive ? activeClass : notActiveClass)}>playlists</NavLink>
+                <NavLink to="/user-tracks" className={({ isActive }) => (isActive ? activeClass : notActiveClass)}>user-tracks</NavLink>
+            </ul>
         </nav>
     );
 }
